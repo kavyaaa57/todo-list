1022cacfcd53a3cf4debd4a9658750ee23fb8647
Kavya <kavyavarshini517@gmail.com>
2024-10-21 00:39:02 +0530

    Build project for deployment-ver 2

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home'; 
 import TodoList from './components/TodoList/TodoList';
 import CompletedTasks from './components/CompletedTasks/CompletedTasks';
 import PendingTasks from './components/PendingTasks/PendingTasks';
+import Home from './components/Home/home';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
         <Route path="/TodoList" element={<TodoList />} />
         <Route path="/completed-tasks" component={CompletedTasks} />
         <Route path="/pending-tasks" component={PendingTasks} />
